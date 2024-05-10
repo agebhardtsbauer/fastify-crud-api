2601aba7ada8fc885700008f345da61b5142d7e7
Alan Gebhardtsbauer <alangebhardtsbauer@MacBook-Air-4.local>
2024-05-10 12:49:45 -0400

    fix spacing of sidenav

--- articles.js
+++ articles.js
@@ -1,5 +1,10 @@
 let articles = [
-  { id: "1", title: "Article 1", content: "Content 1 Dogs" },
+  {
+    id: "1",
+    title: "Article 1",
+    content:
+      "Dogs, often touted as man's best friend, have held a special place in human society for thousands of years. From their earliest domestication, likely stemming from wolves, to the diverse array of breeds we see today, dogs have evolved alongside us, becoming indispensable companions in our lives. Their loyalty, affection, and unwavering devotion make them cherished members of countless families worldwide</br>One of the most remarkable aspects of dogs is their incredible adaptability. Whether it's herding sheep on a farm, assisting police officers in their duties, guiding individuals with disabilities, or simply providing comfort and companionship in the home, dogs effortlessly fill a variety of roles. This versatility speaks to the deep bond forged between humans and dogs over centuries of companionship and cooperation.</br>Beyond their practical roles, dogs bring immense joy and enrichment to our lives. Their playful antics, boundless energy, and infectious enthusiasm have a way of lifting our spirits even on the gloomiest of days. The simple act of taking a dog for a walk can turn into an adventure, with their keen senses leading the way and their wagging tails expressing pure joy in the moment.</br>Furthermore, the relationship between humans and dogs is not just one-sided; it's mutually beneficial. Scientific studies have shown that owning a dog can have numerous positive effects on human health, including reducing stress, lowering blood pressure, and boosting mood. In return for our care and companionship, dogs offer us unconditional love, companionship, and a profound sense of connection that transcends words. Truly, dogs are more than just pets; they are beloved members of our families and integral parts of our lives.",
+  },
   { id: "2", title: "Article 2", content: "Content 2 Cats" },
   { id: "3", title: "Article 3", content: "Content 3 Cats" },
 ];
